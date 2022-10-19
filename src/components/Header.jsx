5c73769076d714eac62d1b298fb2636f267f64cf
morphalex90 <piero.nanni@gmail.com>
2022-10-19 16:18:05 +0100

    add different title if it's homepage, fix mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,33 @@
 import Menu from '@/components/Menu';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 function Header() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const router = useRouter();
+
     return (
         <header className="header">
             <div className="header__container">
 
                 <div className="header__title">
-                    <Link href="/">Tati&rsquo;s Wedding</Link>
+                    <Link href="/">
+                        {router.pathname === '/' ? 'Welcome to Ostuni' : 'Tati\'s Wedding'}
+                    </Link>
                     <span>- Benvenuti al nostro matrimonio</span>
                 </div>
 
-                <div className="header__menu">
+                <div className={'header__menu' + (isMobileMenuOpen ? ' is-open' : '')}>
                     <Menu position="header" />
+                </div>
+
+                <div className="header__mobile-menu-toggle">
+                    <button type="button" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
                 </div>
 
             </div>
